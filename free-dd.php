86e0e245b2fa84474ae7a3cc9420ad79cebfa165
arlad <info@arlad.ru>
2017-07-30 14:30:13 +0300

    Blog page

---
+++ free-dd.php
@@ -0,0 +1,115 @@
+<?php include('includes/head.php'); ?>
+<?php include('includes/header.php'); ?>
+
+	<!-- Main Banner -->
+	<div class="main-header" style="background-image: url(images/temp/bg-1.jpg);">
+		<div class="header-inner">
+			<div class="header-pre-title"><span>FOREVER FREE template builder</span></div>
+			<h1 class="header-title">FREE Drag&Drop Email Builder</h1>
+		</div>
+	</div>
+	<!--/ Main Banner -->
+
+	<!-- Section Post -->
+	<section class="section section-post">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<h2 class="post-title">Amazing Features Blog Posts About Email Growth</h2>
+
+					<p class="first-letter">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin leo arcu, hendrerit quis pharetra id, condime
+						in erat. Quisque dignissim nisl nec placerat fringilla. Integer at dignissim tortor. Maecenas ornare ut metus
+						isque ultricies. Etiam non posuere justo, vitae lobortis ligula. Suspendisse mattis viverra diam in commodo.
+						Sed egestas mauris vel massa tincidunt, vel mollis lectus aliquam. Curabitur a feugiat metus.
+					</p>
+
+					<p>
+						Donec placerat turpis nulla, vel pharetra purus sodales at. Aliquam maximus ex et cursus mattis. Vestibulum a congue nisl.
+						Donec semper ultricies orci sit amet auctor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id orci commodo,
+						porta metus id, feugiat libero. Duis tincidunt quis metus vitae auctor.
+					</p>
+
+					<p>
+						Phasellus ut iaculis sapien, quis blandit felis. Nullam a tortor feugiat, auctor nunc a, commodo diam. Aliquam erat volutpat.
+						Integer varius pharetra leo at sollicitudin. Donec at ipsum sit amet dolor accumsan aliquet. Pellentesque habitant morbi tristique
+						senectus et netus et malesuada fames ac turpis egestas. Morbi pharetra tortor et lobortis hendrerit. Nunc vel tincidunt nulla,
+						et accumsan dui. Nulla est dolor, mollis ut neque ac, consectetur feugiat metus. Nunc rhoncus, odio a semper imperdiet,
+						nisi lacus vehicula nisl, vitae semper libero lectus in eros. Quisque sodales pellentesque tellus, nec scelerisque magna iaculis sed.
+						Fusce congue dapibus dolor, ac sodales nulla facilisis quis. Ut volutpat, ligula vel porta porta, risus ex finibus magna, sit amet
+						efficitur justo tellus non purus. Fusce ultricies cursus nibh eget ullamcorper.
+					</p>
+
+					<p>
+						Sed sem felis, laoreet at leo in, tempus sodales felis. Duis venenatis justo ut nibh iaculis, at malesuada risus dapibus.
+						Duis ornare ut tortor et feugiat. Donec egestas risus magna, a tincidunt dolor laoreet ac. Phasellus sed fringilla orci.
+						Nunc vehicula euismod felis, ac cursus tellus efficitur pellentesque. Integer pellentesque nulla massa, ut congue justo laoreet ut.
+						Praesent dapibus iaculis dignissim. Ut ut ultricies sem. Praesent pulvinar magna sed est pellentesque semper. Ut sed mauris a ligula
+						tristique tincidunt non eget justo. Nam luctus, nibh id iaculis aliquam, elit libero
+					</p>
+
+					<div class="blockquote">
+						<blockquote>
+							Morbi congue sem felis, non luctus diam molestie sit amet. Suspendisse potenti. Nullam eu velit ut velit porta tempor ut id nibh.
+							Sed pharetra mi id rhoncus commodo. Maecenas feugiat varius metus, nec luctus nunc condimentum a. Quisque blandit lacus dui.
+						</blockquote>
+
+						<span>Andrei marin - bIG BANG THEMES</span>
+					</div>
+
+					<p>
+						Donec at ipsum sit amet dolor accumsan aliquet. Pellentesque habitant morbi tristique senectus et netus et malesuT fames
+						ac turpis egestas. Morbi pharetra tortor et lobortis hendrerit. Nunc vel tincidunt nulla, et accumsan dui. Nulla est dolor,
+						mollis ut neque ac, consectetur feugiat metus. Nunc rhoncus, odio a semper imperdiet, nisi lacus vehicula nisl, vitae semper
+						libero lectus in eros. Quisque sodales pellentesque tellus, nec scelerisque magna iaculis sed. Fusce congue dapibus dolor,
+						ac sodales nulla facilisis quis. Ut volutpat, ligula vel porta porta, risus ex finibus magna, sitera amet efficitur justo
+						tellus non purus. Fusce ultricies cursus nibh eget ullamcorper.
+					</p>
+
+					<p>
+						Nam semper tellus diam, vitae semper nisi hendrerit ac. Maecenas pharetra erat purus, at fringilla nunc imperdietop quis.
+						Curabitur efficitur pharetra gravida. Praesent vestibulum malesuada risurs, quis laorewwret mauris posuere eu.
+						Suspendisse rutrum sem placerat, hendrerit nunc in, posuere ante. Etiam et sollicitudin ante. Aliquam eget nulla in justo
+						auctor faucibus ac et lectus. Morbi eu congue elit. Aliquam aliquet mauris at augue eleifend consectetur. Duis vitae laoreet augue,
+						eget consequat dui. Pellentesque felis elit, aliquet ut tincidunt vel, sagittis at augue. In aliquet pellentesque placerat.
+					</p>
+
+					<div class="post-footer">
+						<div class="row">
+							<div class="col-xs-9">
+								<!-- Tags -->
+								<div class="post-taglist">
+									<a href="#">builder</a>
+									<a href="#">email</a>
+									<a href="#">marketing</a>
+									<a href="#">email marketing</a>
+									<a href="#">wordpress</a>
+									<a href="#">website</a>
+								</div>
+								<!--/ Tags -->
+							</div>
+
+							<div class="col-xs-3">
+								<div class="text-right">
+									<!-- Social Share Buttons -->
+									<div class="post-social">
+										<a href="#"><i class="fa fa-share-alt"></i></a>
+
+										<ul>
+											<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+											<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+										</ul>
+									</div>
+									<!--/ Social Share Buttons -->
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!--/ Section Post -->
+
+<?php include('includes/footer.php'); ?>
+<?php include('includes/scripts.php'); ?>
