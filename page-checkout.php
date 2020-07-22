90224fcb3ef5f79065f80f9b07d2296666029684
Kemo <bivol.leonid@gmail.com>
2020-07-22 21:19:30 +0200

    Page Checkout

---
+++ page-checkout.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<!-- Mobile Specific Meta -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+	<!-- Pingbacks -->
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+	<link rel="canonical" href="<?php the_permalink() ?>" />
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class('checkout-page')?>>
+
+<?php require_once('theme_includes/header_nav.php'); ?>
+
+<?php get_footer(); ?>
+
+<?php wp_footer( );?>
+
+</body>
+</html>
