a3fa6ce8725e9e303c65ecf9e1e31122c88c80a4
arlad <info@betemplates.com>
2020-07-27 23:53:30 +0300

    Recurly setup

--- js/recurly-api.js
+++ js/recurly-api.js
@@ -1,3 +1,87 @@
 jQuery(document).ready(function($) {
     var recurlyPublicApiKey = 'ewr1-zt3dc3P4KgI3lmRgmvquit';
+
+    recurly.configure(recurlyPublicApiKey);
+
+    const elements = recurly.Elements();
+
+    //const cardElement = elements.CardElement();
+    const cardNumberElement = elements.CardNumberElement({
+        style: {
+            placeholder: {
+                content: 'Card Number'
+            }
+        }
+    });
+
+    const cardMonthElement = elements.CardMonthElement({
+        style: {
+            placeholder: {
+                content: 'Expiry Month'
+            }
+        }
+    });
+
+    const cardYearElement = elements.CardYearElement({
+        style: {
+            placeholder: {
+                content: 'Expiry Year'
+            }
+        }
+    });
+
+    const cardCvvElement = elements.CardCvvElement({
+        style: {
+            placeholder: {
+                content: 'CVV'
+            }
+        }
+    });
+
+    cardNumberElement.attach('#recurly-cardNumber');
+    cardMonthElement.attach('#recurly-cardMonth');
+    cardYearElement.attach('#recurly-cardYear');
+    cardCvvElement.attach('#recurly-cardCvv');
+
+
+
+
+
+
+
+
+    document.querySelector('#my-form').addEventListener('submit', function (event) {
+        const form = this;
+        event.preventDefault();
+        recurly.token(elements, form, function (err, token) {console.log(err, token);
+            if (err) {
+                // handle error using err.code and err.fields
+            } else {
+                // recurly.js has filled in the 'token' field, so now we can submit the
+                // form to your server
+                form.submit();
+            }
+        });
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
--- page-checkout.php
+++ page-checkout.php
@@ -19,9 +19,196 @@ Template Name: Page Checkout
 */
 ?>
 
-<?php require_once('theme_includes/header_nav.php'); ?>
+<section class="section">
+	<div class="container">
 
-<?php get_footer(); ?>
+
+
+		<form id="my-form">
+			<div class="row">
+				<div class="col-sm-6 col-md-4">
+					<input class="form-control" type="text" data-recurly="first_name" placeholder="First Name">
+				</div>
+
+				<div class="col-sm-6 col-md-4">
+					<input class="form-control" type="text" data-recurly="last_name" placeholder="Last Name">
+				</div>
+
+				<div class="col-sm-6 col-md-4">
+					<div class="form-control" id="recurly-cardNumber"></div>
+				</div>
+
+				<div class="col-sm-6 col-md-4">
+					<div class="form-control" id="recurly-cardMonth"></div>
+				</div>
+
+				<div class="col-sm-6 col-md-4">
+					<div class="form-control" id="recurly-cardYear"></div>
+				</div>
+
+				<div class="col-sm-6 col-md-4">
+					<div class="form-control" id="recurly-cardCvv"></div>
+				</div>
+			</div>
+
+
+
+
+
+
+
+
+
+			<!-- Recurly.js will update this field automatically -->
+			<input type="hidden" name="recurly-token" data-recurly="token">
+
+			<button class="btn">submit</button>
+		</form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	</div>
+</section>
+
+<!-- Section Pricing -->
+<section class="section section-pricing">
+	<div class="container">
+		<div class="section-header text-center">
+			<div class="section-pre-title"><span>AFFORDABLE PLANS</span></div>
+			<h2 class="section-title">Atractive <em>pricing plan!</em></h2>
+
+			<div class="section-description">
+				<p>
+					<strong>50% off</strong> for the first 500 subscribers! A limited number of subscribers can enjoy a huge discount for life.
+					You will be paying <strong class="color-green">$7.50</strong> instead of <strong>$14</strong> if you are lucky enough to be among
+					the first 500 subscribers. Save up to <strong class="color-green">$90</strong> per year and access unlimited ways to boost
+					your marketing campaigns!
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div id="pricing" class="pricing-plans">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4 col-sm-6">
+					<div class="pricing-item">
+						<div class="item-badge">best choice</div>
+						<h2 class="item-name">Free</h2>
+						<div class="item-price"><sup>$</sup>0</div>
+						<div class="item-description">100% free forever</div>
+
+						<ul class="item-features">
+							<li>Free forever</li>
+							<li>15 test modules</li>
+							<li>No new modules</li>
+							<li>No support</li>
+							<li>No new templates</li>
+							<li>Unlimited exports</li>
+							<li>No private conversation</li>
+						</ul>
+
+						<div class="buttons">
+							<a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect btn">Choose</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-6">
+					<div class="pricing-item">
+						<div class="item-badge">best choice</div>
+						<h2 class="item-name">Standard</h2>
+						<div class="item-price"><sup>$</sup>20</div>
+						<div class="item-description">Per month</div>
+
+						<ul class="item-features">
+							<li>One time fee</li>
+							<li>200 modules</li>
+							<li>No new modules</li>
+							<li>6 months of support</li>
+							<li>No new templates</li>
+							<li>Unlimited exports</li>
+							<li>No private conversation</li>
+						</ul>
+
+						<div class="buttons">
+							<a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect btn">Choose</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-4 col-sm-12">
+					<div class="pricing-item active">
+						<div class="item-badge">best choice</div>
+						<h2 class="item-name">PRO</h2>
+						<div class="item-price"><sup>$</sup>10</div>
+						<div class="item-description">Per month</div>
+
+						<ul class="item-features">
+							<li>Monthly subscription fee</li>
+							<li>+800 modules</li>
+							<li>New modules every week</li>
+							<li>Continuous support</li>
+							<li>1 new template per month</li>
+							<li>Unlimited exports</li>
+							<li>10 minutes private talk</li>
+						</ul>
+
+						<div class="buttons">
+							<?php
+							//$current_user_id = get_current_user_id();
+							//update_option("bbt_".$current_user_id."_license_builder", "");
+							//update_user_option( $current_user_id, 'payment_info', '');
+							//bbtb_pricing_button($is_template);
+							?>
+							<a href="#" class="mdl-button mdl-js-button mdl-js-ripple-effect btn">Choose</a>
+						</div>
+					</div>
+				</div>
+
+				<?php if(!is_user_logged_in()): ?>
+					<div class="col-md-6 col-sm-6">
+						<?php get_template_part('login', 'form');?>
+					</div>
+				<?php else: ?>
+					<?php //if(is_bbtb_child_items_exists() && bbtb_child_check_valid_user_key() != 'yes'):?>
+					<div class="col-md-12 col-sm-12">
+						<?php get_template_part('validate', 'token'); ?>
+					</div>
+					<?php //elseif(isset($_GET['token']) && !empty($_GET['token']) && $is_template):?>
+					<div class="bbt-start-subscription">
+						<div class="col-md-12 col-sm-12">
+							<?php //bbt_start_subscription($_GET['buy']); ?>
+						</div>
+					</div>
+					<?php //endif; ?>
+				<?php endif;?>
+
+			</div>
+		</div>
+	</div>
+</section>
+<!--/ Section Pricing -->
 
 <?php wp_footer( );?>
 
